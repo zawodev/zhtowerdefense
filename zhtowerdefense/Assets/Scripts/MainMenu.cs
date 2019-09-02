5f0773933d0cfe2471a72d2368823326a61567e1
z4v0 <alikdelta@wp.pl>
2019-09-02 20:08:58 +0200

    No elo

---
+++ zhtowerdefense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ChangeScene(string name){
+        SceneManager.LoadScene(name);
+    }
+
+
+}
