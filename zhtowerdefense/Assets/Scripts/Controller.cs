6542e9e063c5719491aa0fdac307205fc53a1c9b
z4v0 <alikdelta@wp.pl>
2019-09-04 20:01:23 +0200

    bierz to :D

--- zhtowerdefense/Assets/Scripts/Controller.cs
+++ zhtowerdefense/Assets/Scripts/Controller.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-    float speed;
-
-    void Awake() {
-        speed = 40f;
-    }
+    public float speed;
+    public Rigidbody2D rb;
 
     void Update() {
-        if(Input.GetKey("w")) transform.position += transform.up * speed * Time.deltaTime;
+        /*if(Input.GetKey("w")) transform.position += transform.up * speed * Time.deltaTime;
         if(Input.GetKey("s")) transform.position -= transform.up * speed * Time.deltaTime;
         if(Input.GetKey("a")) transform.position -= transform.right * speed * Time.deltaTime;
-        if(Input.GetKey("d")) transform.position += transform.right * speed * Time.deltaTime;
+        if(Input.GetKey("d")) transform.position += transform.right * speed * Time.deltaTime;/*/
+
+        // musiałem zakomentowac sry xD
+
+        MoveCharacter(new Vector2 (Input.GetAxis("Horizontal"), 0));
+    }
+    public void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
     }
 }
